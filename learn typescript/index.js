558b628ac868baca51ad9a9e1895b6f574aaf8a1
Akshit <aa.opash@gmail.com>
2026-01-07 18:35:35 +0530

    new

---
+++ learn typescript/index.js
@@ -0,0 +1,5 @@
+function greet(person) {
+    return "Hello, ".concat(person, ", welcome to TypeScript!");
+}
+var username = "greet(person: string): strinng ";
+console.log(greet(username));
---
+++ learn typescript/index.ts
@@ -0,0 +1,7 @@
+
+
+function greet(person: string): string {
+    return `Hello, ${person}, welcome to TypeScript!`
+}
+const username : string = "greet(person: string): strinng "
+console.log(greet(username))
