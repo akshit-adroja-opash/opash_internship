c8221f6cb4e8b572bfb9d12fbaf30c58338807b9
Akshit adroja <aa.opash@gmail.com>
2026-03-12 11:58:13 +0530

    .

--- nextjs/app/(users)/contact/page.jsx
+++
@@ -1,5 +0,0 @@
-const Contact = async () => {
- return <h1>About contact</h1>
-}
-
-export default Contact;
--- nextjs/app/admin/about/page.jsx
+++
@@ -1,7 +0,0 @@
-const admin = () => {
-    return (
-        
-            <h1>Admin About Us</h1>
-    )}
-
-export default admin;
